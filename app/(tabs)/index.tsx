478b48ccc6ddab1edbf9423e777af70dea61680a
Mattias Arwidson <mattias@actionawareness.se>
2024-09-20 23:28:27 +0200

    prettier

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,71 +1,88 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
+const lexa1 = () => {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <View
+      style={[
+        styles.defaultViewMain,
+        { backgroundColor: "lightblue", flex: 1 },
+      ]}
+    >
+      <View style={[styles.defaultView, { paddingTop: 0, height: 90 }]}>
+        <View
+          style={[
+            styles.defaultView,
+            {
+              backgroundColor: "red",
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+            },
+          ]}
+        >
+          <Text style={[styles.defaultText, {}]}>RÖD</Text>
+        </View>
+      </View>
+
+      <View style={[styles.defaultView, styles.defaultViewRow, {}]}>
+        <View
+          style={[
+            styles.defaultView,
+            { backgroundColor: "green", height: 90, flex: 1 },
+          ]}
+        >
+          <Text style={[styles.defaultText, {}]}>GRÖN</Text>
+        </View>
+        <View style={[styles.defaultView]} />
+        <View
+          style={[styles.defaultView, { backgroundColor: "yellow", flex: 1 }]}
+        ></View>
+      </View>
+
+      <View style={[styles.defaultView, styles.defaultViewRow, { height: 45 }]}>
+        <View
+          style={[styles.defaultView, { backgroundColor: "red", flex: 3 }]}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-      <HelloWave />        
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+        <View style={[styles.defaultView]} />
+        <View
+          style={[styles.defaultView, { backgroundColor: "black", flex: 1 }]}
+        />
+      </View>
+
+      <View style={{ flex: 1, justifyContent: "flex-end" }}>
+        <View
+          style={[
+            styles.defaultView,
+            {
+              backgroundColor: "orange",
+              height: 30,
+              alignItems: "flex-end",
+              justifyContent: "center",
+            },
+          ]}
+        >
+          <Text style={[styles.defaultText, {}]}>ORANGE</Text>
+        </View>
+      </View>
+    </View>
   );
-}
+};
+
+export default lexa1;
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  defaultText: {
+    fontSize: 20,
+    paddingHorizontal: 4,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  defaultViewMain: {
+    padding: 2,
+    flexDirection: "column",
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  defaultViewRow: {
+    flexDirection: "row",
+  },
+  defaultView: {
+    padding: 2,
   },
 });
